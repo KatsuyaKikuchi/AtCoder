5b9a4c35a1ccf1de01eaef5e5da2c70ff718a611
菊池克弥 <katsuyakikuchi@kikuchikatsuwatarunoMacBook-Pro.local>
2018-12-22 17:08:33 +0900

    first commit

---
+++ G029A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll,ll> pll;
+
+#define FOR(i,n,m) for(ll (i)=(m);(i)<(n);++(i))
+#define REP(i,n) FOR(i,n,0)
+#define OF64 std::setprecision(10)
+
+const ll MOD = 1000000007;
+const ll INF = (ll)1e15;
+
+int main()
+{
+    string S;
+    cin>>S;
+    ll w=0,ans=0;
+    
+    REP(i,S.length())
+    {
+        if(S[i]=='W')
+        {
+            ans+=(i-w);
+            w++;
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
