80830e00af17342e2cc8778d60e33b703560680c
菊池克弥 <katsuyakikuchi@kikuchikatsuwatarunoMacBook-Pro.local>
2018-12-22 23:07:03 +0900

    caddi 2018 file

---
+++ CADDi/2018/A.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll,ll> pll;
+
+#define FOR(i,n,m) for(ll (i)=(m);(i)<(n);++(i))
+#define REP(i,n) FOR(i,n,0)
+#define OF64 std::setprecision(10)
+
+const ll MOD = 1000000007;
+const ll INF = (ll)1e15;
+
+vector<int> primeList;
+bool isPrime[1000005];
+void calcPrime()
+{
+    isPrime[0]=isPrime[1]=false;
+    for(int i=2;i<1000005;i+=1)
+    {
+        if(isPrime[i])
+        {
+            continue;
+        }
+        primeList.push_back(i);
+        for(int j=i;j<1000005;j+=i)
+        {
+            isPrime[j]=true;
+        }
+    }
+}
+
+ll pn[1000005];
+
+int main()
+{
+    ll N,P;
+    cin>>N>>P;
+    calcPrime();
+    int i=0;
+    while(i<primeList.size())
+    {
+        ll p=primeList[i];
+        if(P%p==0)
+        {
+            P/=p;
+            pn[p]++;
+            continue;
+        }
+        i++;
+        if(P==1)
+        {
+            break;
+        }
+    }
+    ll sum=P;
+    REP(j,1000005)
+    {
+        ll n =pn[j]/N;
+        if(n==0)continue;
+        sum*=(j*n);
+    }
+    cout<<sum<<endl;
+    return 0;
+}
---
+++ CADDi/2018/B.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll,ll> pll;
+
+#define FOR(i,n,m) for(ll (i)=(m);(i)<(n);++(i))
+#define REP(i,n) FOR(i,n,0)
+#define OF64 std::setprecision(10)
+
+const ll MOD = 1000000007;
+const ll INF = (ll)1e15;
+
+ll a[100005];
+
+int main()
+{
+
+    int N;
+    cin>>N;
+    REP(i,N)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+N);
+    return 0;
+}
