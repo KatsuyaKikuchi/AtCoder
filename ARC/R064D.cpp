ed206b7f583180e652838e90daf8166c0ed58b67
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2018-12-25 20:13:51 +0900

    solve arc064d

--- ARC/R064D.cpp
+++ ARC/R064D.cpp
@@ -13,5 +13,23 @@ const ll INF = (ll)1e15;
 
 int main()
 {
+    string S;
+    cin>>S;
+    string ans="First";
+    if(S[0]==S[S.length()-1])
+    {
+        if(S.length()%2==1)
+        {
+            ans="Second";
+        }
+    }
+    else
+    {
+        if(S.length()%2==0)
+        {
+            ans="Second";
+        }
+    }
+    cout<<ans<<endl;
     return 0;
 }
