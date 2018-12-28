75ff9388e417559fc348ed39f808dda4a285185b
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2018-12-28 21:48:04 +0900

    solve aoj and update union-find

--- Algorithm/unionFind.cpp
+++ Algorithm/unionFind.cpp
@@ -6,9 +6,11 @@ struct UnionFind
     {
         mN = n;
         mNodeTbl.resize(mN);
+        mRankTbl.resize(mN);
         REP(i, mN)
         {
             mNodeTbl[i] = i;
+            mRankTbl[i] = 0;
         }
     }
     int find(int x)
@@ -17,17 +19,29 @@ struct UnionFind
             return x;
         return mNodeTbl[x] = find(mNodeTbl[x]);
     }
-    bool same(int x,int y)
+    bool same(int x, int y)
     {
-        return find(x)==find(y);
+        return find(x) == find(y);
     }
-    void unit(int x,int y)
+    void unit(int x, int y)
     {
-        x=find(x);
-        y=find(y);
-        mNodeTbl[x]=y;
+        x = find(x);
+        y = find(y);
+        if (x == y)
+            return;
+        if (mRankTbl[x] < mRankTbl[y])
+        {
+            mNodeTbl[x] = y;
+        }
+        else
+        {
+            mNodeTbl[y] = x;
+            if (mRankTbl[x] == mRankTbl[y])
+                mRankTbl[x]++;
+        }
     }
 
     int mN;
     vector<int> mNodeTbl;
+    vector<int> mRankTbl;
 };
