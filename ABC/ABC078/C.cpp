f78092a72369d74357f0da6c4bf6decef0885298
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2019-01-14 18:46:52 +0900

    solve abc078

---
+++ ABC/ABC078/C.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, n, m) for (ll(i) = (m); (i) < (n); ++(i))
+#define REP(i, n) FOR(i, n, 0)
+#define OF64 std::setprecision(10)
+
+const ll MOD = 1000000007;
+const ll INF = (ll)1e15;
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    cout << (100 * N + 1800 * M) * pow(2, M) << endl;
+    return 0;
+}
