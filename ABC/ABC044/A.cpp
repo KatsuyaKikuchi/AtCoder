b01e9b96760f8ebf1eea087a9d31eb322a43cee8
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2019-01-16 23:38:57 +0900

    solve abc044

---
+++ ABC/ABC044/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, n, m) for (ll(i) = (m); (i) < (n); ++(i))
+#define REP(i, n) FOR(i, n, 0)
+#define OF64 std::setprecision(10)
+
+const ll MOD = 1000000007;
+const ll INF = (ll)1e15;
+
+int main()
+{
+    ll N, K, X, Y;
+    cin >> N >> K >> X >> Y;
+    cout << N * X - std::max((ll)0, N - K) * (X - Y) << endl;
+    return 0;
+}
