73609cf2cae8aad75a553fb6ea9a72f333b8adb0
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2019-02-06 22:15:51 +0900

    solve abc069

---
+++ ABC/ABC069/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, n, m) for (ll(i) = (m); (i) < (n); ++(i))
+#define REP(i, n) FOR(i, n, 0)
+#define OF64 std::setprecision(10)
+
+const ll MOD = 1000000007;
+const ll INF = (ll)1e15;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    cout << (n - 1) * (m - 1) << endl;
+    return 0;
+}
