ea1726402c5b727f16b4484e6d8a50b717a3b146
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2019-02-26 21:30:10 +0900

    solve abc060

---
+++ ABC/ABC060/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, n, m) for (ll(i) = (m); (i) < (n); ++(i))
+#define REP(i, n) FOR(i, n, 0)
+#define OF64 std::setprecision(10)
+
+const ll MOD = 1000000007;
+const ll INF = (ll)1e15;
+
+int main()
+{
+    string A, B, C;
+    cin >> A >> B >> C;
+    if (A[A.length() - 1] == B[0] && B[B.length() - 1] == C[0])
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
