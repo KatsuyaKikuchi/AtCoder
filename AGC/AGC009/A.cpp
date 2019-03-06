42cceb441f7ad6a79e40bec4b348ddef078c7825
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2019-03-06 21:15:36 +0900

    solve agc009

---
+++ AGC/AGC009/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, n, m) for (ll(i) = (m); (i) < (n); ++(i))
+#define REP(i, n) FOR(i, n, 0)
+#define OF64 std::setprecision(10)
+
+const ll MOD = 1000000007;
+const ll INF = (ll)1e15;
+
+ll A[100005];
+ll B[100005];
+
+int main()
+{
+    int N;
+    cin >> N;
+    REP(i, N)
+    {
+        cin >> A[i] >> B[i];
+    }
+
+    ll sum = 0;
+    for (int i = N - 1; i >= 0; --i)
+    {
+        ll t = (sum + A[i] + B[i] - 1) / B[i];
+        sum += B[i] * t - (A[i] + sum);
+    }
+    cout << sum << endl;
+    return 0;
+}
