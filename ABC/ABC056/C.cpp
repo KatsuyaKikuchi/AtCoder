b136bd7087ea9343a647dac68f84608063c09099
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2019-03-10 21:02:52 +0900

    solve abc056

---
+++ ABC/ABC056/C.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, n, m) for (ll(i) = (m); (i) < (n); ++(i))
+#define REP(i, n) FOR(i, n, 0)
+#define OF64 std::setprecision(10)
+
+const ll MOD = 1000000007;
+const ll INF = (ll)1e15;
+
+int main()
+{
+    ll X;
+    cin >> X;
+    ll sum = 0;
+    REP(i, X + 1)
+    {
+        sum += i;
+        if (X <= sum)
+        {
+            cout << i << endl;
+            break;
+        }
+    }
+    return 0;
+}
