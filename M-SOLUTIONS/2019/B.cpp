dbe689d3c4448819e941c934dc352e134dfdaa82
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2019-06-02 20:50:53 +0900

    solve m-solutions 2019

---
+++ M-SOLUTIONS/2019/B.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, n, m) for (ll(i) = (m); (i) < (n); ++(i))
+#define REP(i, n) FOR(i, n, 0)
+#define OF64 std::setprecision(10)
+
+const ll MOD = 1000000007;
+const ll INF = (ll)1e15;
+
+int main()
+{
+    string S;
+    cin >> S;
+    int n = 0;
+    REP(i, S.length())
+    {
+        if (S[i] == 'o')
+            n++;
+    }
+
+    if (n + 15 - S.length() >= 8)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
