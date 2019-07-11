9ab8c275226b474ecadd5ba494c852b647a7936e
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2019-07-11 21:31:56 +0900

    solve chokudai speed run

---
+++ chokudai speed run/02/F.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, n, m) for (ll(i) = (m); (i) < (n); ++(i))
+#define REP(i, n) FOR(i, n, 0)
+#define OF64 std::setprecision(10)
+
+const ll MOD = 1000000007;
+const ll INF = (ll)1e15;
+
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<pll> C;
+    REP(i, N)
+    {
+        ll a, b;
+        cin >> a >> b;
+        C.push_back(pll(std::min(a, b), std::max(a, b)));
+    }
+    sort(C.begin(), C.end());
+    C.erase(unique(C.begin(), C.end()), C.end());
+    cout << C.size() << endl;
+
+    return 0;
+}
---
+++ chokudai speed run/02/G.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, n, m) for (ll(i) = (m); (i) < (n); ++(i))
+#define REP(i, n) FOR(i, n, 0)
+#define OF64 std::setprecision(10)
+
+const ll MOD = 1000000007;
+const ll INF = (ll)1e15;
+
+ll gcd(ll a, ll b)
+{
+    if (a < b)
+        swap(a, b);
+    ll c = a % b;
+    if (c == 0)
+        return b;
+    return gcd(b, c);
+}
+
+int main()
+{
+    ll N;
+    cin >> N;
+    REP(i, N)
+    {
+        ll a, b;
+        cin >> a >> b;
+        cout << gcd(a, b) << endl;
+    }
+    return 0;
+}
